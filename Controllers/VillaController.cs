b14ac4c917e06d095337d685d44e8f63ff6a2718
wilfer andres <wilferandres2928@gmail.com>
2023-04-07 22:47:20 -0500

    cambios en la estructura de la información

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,22 @@
+﻿using MagiVilla_api.modelos;
+using MagiVilla_api.modelos.dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagiVilla_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto>
+        {
+            new VillaDto {Id = 1, nombre = "vista a la piscina"},
+            new VillaDto {Id = 2, nombre = "vista a la playa"}
+        };
+        }
+    }
+}
