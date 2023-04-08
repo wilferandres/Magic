10b9048aab4ed5880d57f54edbcf088e3256873d
wilfer andres <wilferandres2928@gmail.com>
2023-04-07 23:03:32 -0500

    refactorizando código

---
+++ datos/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagiVilla_api.modelos.dto;
+
+namespace MagiVilla_api.datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto>
+        {
+            new VillaDto { Id = 1, nombre = "wilfer Andres" },
+            new VillaDto { Id = 2, nombre = "lilibet bermeo" },
+            new VillaDto { Id = 3, nombre = "freiman ordoñez"}
+        };
+    }
+}
